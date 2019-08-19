15b39fc657fe23c058cf16b247a99710490192ef
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-08-18 22:07:53 -0600

    fixes spelling errors

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,8 +4,8 @@ describe "Merchants API" do
   it "sends a list of merchants" do
     create_list(:merchant, 3)
 
-    get 'api/v1/merchants'
+    get '/api/v1/merchants'
 
-    expect(reponse.to be_succesful)
+    expect(response).to be_successful
   end
 end
